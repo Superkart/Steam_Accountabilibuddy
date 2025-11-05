606dfdd0e68e0988beefebf939c9a23aed2a4bb7
Abhishek <walvekarabhishek701@gmail.com>
2025-11-05 01:14:44 -0600

    add spring boilerplate

---
+++ backend/steamaccountabilibuddy/src/main/java/com/cs484/steamaccountabilibuddy/SteamaccountabilibuddyApplication.java
@@ -0,0 +1,13 @@
+package com.cs484.steamaccountabilibuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamaccountabilibuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamaccountabilibuddyApplication.class, args);
+	}
+
+}
---
+++ backend/steamaccountabilibuddy/src/test/java/com/cs484/steamaccountabilibuddy/SteamaccountabilibuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs484.steamaccountabilibuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamaccountabilibuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
